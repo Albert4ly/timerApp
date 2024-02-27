52b289f8bee89efc24eed9c7be078bdf9893ca12
Albert <a91614830@gmail.com>
2024-02-27 21:50:59 +0100

    feature:timer-class-with-three-properties-at-constructor-of-timer-class-created

--- js/timer.js
+++ js/timer.js
@@ -1,2 +1,11 @@
 import { hour, minute, second } from "./htmlElementHandles.js";
 
+class Timer {
+   constructor() {
+      this.hours = 0;
+      this.minutes = 0;
+      this.seconds = 0;
+   }
+}
+
+new Timer();
