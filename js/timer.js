2bb222190e6b8ed40c0e2e2661cb72baaf5c7e68
Albert <a91614830@gmail.com>
2024-02-28 22:49:37 +0100

    refactor:Separate-the-logic-for-editing-second-and-minutes-inputs-into-a-separate-func

--- js/timer.js
+++ js/timer.js
@@ -6,67 +6,54 @@ const MAX_MINUTES = 59;
 const MAX_SECONDS = 59;
 
 class Timer {
-   constructor() {
-      this.hours = 0;
-      this.minutes = 0;
-      this.seconds = 0;
+	constructor() {
+		this.hours = 0;
+		this.minutes = 0;
+		this.seconds = 0;
 
-      this.toNumberConverted = 0;
+		this.toNumberConverted = 0;
 
-      this.listenerFunc();
-   }
+		this.listenerFunc();
+	}
 
-   listenerFunc() {
-      hour.addEventListener("change", (e) => this.editHourInput(e));
-      minute.addEventListener("change", (e) => this.editMinuteInput(e));
-      second.addEventListener("change", (e) => this.editSecondInput(e));
-   }
+	listenerFunc() {
+		hour.addEventListener("change", (e) => this.editHourInput(e));
+		minute.addEventListener("change", (e) => this.editMinuteInput(e));
+		second.addEventListener("change", (e) => this.editSecondInput(e));
+	}
 
-   editMinuteInput(e) {
+	editMinuteInput(e) {
 		this.fromStrValueToNumberConverted(e);
+      this.editValue(MAX_MINUTES, this.minutes, minute);
+	}
 
-		if (this.toNumberConverted > MAX_MINUTES) {
-			this.minutes = MAX_MINUTES;
-			minute.value = this.minutes;
-		}
+	editSecondInput(e) {
+      this.fromStrValueToNumberConverted(e);
+      this.editValue(MAX_SECONDS, this.seconds, second);
+	}
 
-		if (this.toNumberConverted < MAX_MINUTES) {
-			if (this.toNumberConverted > NINE_NUMBER) {
-				this.minutes = this.toNumberConverted;
-            minute.value = this.minutes;
-			}
-		}
-
-		if (this.toNumberConverted <= NINE_NUMBER) {
-			this.minutes = ZERO_STRING + this.toNumberConverted.toString();
-         minute.value = this.minutes;
-		}
+	fromStrValueToNumberConverted(e) {
+		this.toNumberConverted = Number(e.target.value);
    }
    
-   editSecondInput(e) {
-      this.fromStrValueToNumberConverted(e);
+   editValue(MAX_NUMBER, valueOfTimeUnit, newInputValue) {
+      if (this.toNumberConverted > MAX_NUMBER) {
+         valueOfTimeUnit = MAX_NUMBER;
+         newInputValue.value = valueOfTimeUnit;
+      }
 
-		if (this.toNumberConverted > MAX_SECONDS) {
-			this.seconds = MAX_SECONDS;
-			second.value = this.seconds;
-		}
+      if (this.toNumberConverted < MAX_NUMBER) {
+         if (this.toNumberConverted > NINE_NUMBER) {
+            valueOfTimeUnit = this.toNumberConverted;
+            newInputValue.value = valueOfTimeUnit;
+         }
+      }
 
-		if (this.toNumberConverted < MAX_SECONDS) {
-			if (this.toNumberConverted > NINE_NUMBER) {
-				this.seconds = this.toNumberConverted;
-            second.value = this.seconds;
-			}
-		}
-
-		if (this.toNumberConverted <= NINE_NUMBER) {
-			this.seconds = ZERO_STRING + this.toNumberConverted.toString();
-         second.value = this.seconds;
-		}
+      if (this.toNumberConverted <= NINE_NUMBER) {
+         valueOfTimeUnit = ZERO_STRING + this.toNumberConverted.toString();
+         newInputValue.value = valueOfTimeUnit;
+      }
    }
-
-   fromStrValueToNumberConverted(e) {
-		this.toNumberConverted = Number(e.target.value);
-	}
 }
 
 new Timer();
