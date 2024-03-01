7bb5fc29660a027290eb8b84175b71400f2e710f
Albert <a91614830@gmail.com>
2024-03-01 21:15:39 +0100

    feature:added-handle-to-playBtn

--- js/htmlElementsHandles.js
+++ js/htmlElementsHandles.js
@@ -2,4 +2,6 @@ const hour = document.querySelector(".form__hour");
 const minute = document.querySelector(".form__minute");
 const second = document.querySelector(".form__second");
 
-export { hour, minute, second };
+const runBtn = document.querySelector(".btnsCnt__controlBtnsCnt__btn--run")
+
+export { hour, minute, second, runBtn };
